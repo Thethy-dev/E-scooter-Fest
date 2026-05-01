b3d4352a7d930a36b351f0c99e8e4f3cfb377564
Thethy-dev <thethy.wdev@gmail.com>
2026-05-01 21:55:29 +0300

    ...

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn=document.querySelector(".menu-button");
+const menu=document.querySelector(".menu");
+
+menuBtn.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+}
+);
